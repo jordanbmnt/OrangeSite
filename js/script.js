6aa4ce2cd84acfc26b3bcefbe6ff6b72f7c192d1
[Jordan Beaumont] <[jimb56223@gmail.com]>
2022-06-19 21:03:29 +0200

    Possible final commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
    const floater = document.getElementById("Ellipse");
-    console.log(floater.attributes.fill.value);
-    console.log(window.innerWidth)
+   const sec01 = document.getElementsByClassName("sec01");
+   const sec04 = document.getElementsByClassName("sec04");
+   console.log(sec01)
+//    testing[0].style.borderRadius = ((parseInt(this.scrollY))/100)*360;
+//     console.log(testing[0].style.borderRadius);
     window.addEventListener("scroll", function(event){
         var scroll = this.scrollY;
-        if(window.innerWidth < 700){
-            if(scroll < 900){
-                floater.attributes.fill.value = "#EE5B3C";
-            }else if(scroll < 2750){
-                floater.attributes.fill.value = "#24333F";
-            }else if(scroll < 4000){
-                floater.attributes.fill.value = "#F0FEE4";
-            }
-        }
-    })
+        var height = (sec04[0].offsetHeight);
+        if(scroll < height){
+             floater.attributes.fill.value = "#EE5B3C";
+         }else if(scroll < (height * 3)){
+             floater.attributes.fill.value = "#24333F";
+         }else{
+             floater.attributes.fill.value = "#F0FEE4";
+         }
+    }, 0.1)
