e7830753853e79bd847c072eccc2c06ac6ce3a0b
Jordan Maurice Beaumont <jordanbmnt@gmail.com>
2024-03-26 20:31:28 +0200

    updated js to change colors of floater and changed the style of the footer (padding)

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
-   const floater = document.getElementById("Ellipse");
-   const sec01 = document.getElementsByClassName("sec01");
-   const sec04 = document.getElementsByClassName("sec04");
-   console.log(sec01)
-//    testing[0].style.borderRadius = ((parseInt(this.scrollY))/100)*360;
-//     console.log(testing[0].style.borderRadius);
-    window.addEventListener("scroll", function(event){
-        var scroll = this.scrollY;
-        var height = (sec04[0].offsetHeight);
-        if(scroll < height){
-             floater.attributes.fill.value = "#EE5B3C";
-         }else if(scroll < (height * 3)){
-             floater.attributes.fill.value = "#24333F";
-         }else{
-             floater.attributes.fill.value = "#F0FEE4";
-         }
-    }, 0.1)
+const floater = document.getElementById("Ellipse");
+const sec01 = document.getElementsByClassName("sec01");
+const sec04 = document.getElementsByClassName("sec04");
+console.log(sec01);
+
+window.addEventListener(
+  "scroll",
+  function () {
+    floater.style.transition = "900ms";
+    var scroll = this.scrollY;
+    var height = sec04[0].offsetHeight;
+    if (scroll < height - 30) {
+      floater.attributes.fill.value = "#EE5B3C";
+      floater.style.transform = "";
+    } else if (scroll < (height - 30) * 3) {
+      floater.attributes.fill.value = "#24333F";
+    } else {
+      floater.style.transform = "";
+      floater.attributes.fill.value = "#F0FEE4";
+    }
+  },
+  0.1
+);
